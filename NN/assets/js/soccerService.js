2a93ec7b4afbbd0e0cda7bb0d8f7c0710cb66e54
Johan Israelsson <johan4411@gmail.com>
2021-07-18 20:34:27 +0200

    Delete old folder and unused import

--- NN/assets/js/soccerService.js
+++
@@ -1,6 +0,0 @@
-
-export async function getAllResults(){
-    return await fetch("https://stryk.herokuapp.com/tipset")
-        .then((response) => response.json())
-        .then(data => data.results)
-}
--- Uppgift 3/assets/js/index.js
+++ Uppgift 3/assets/js/index.js
@@ -1,5 +1,4 @@
 import displayStryktipsData from "./display_results.js";
-import fetchStryktipsData from "./fetch_stryktipset.js"
 
 function init(){
     displayStryktipsData()
