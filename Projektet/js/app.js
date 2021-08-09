e6d2a38154fafe89eec437be32d0380be918cede
Johan Israelsson -Desktop <johan4411@gmail.com>
2021-08-09 23:25:10 +0200

    Added js for read less and read more

--- Projektet/js/app.js
+++ Projektet/js/app.js
@@ -50,6 +50,11 @@ function showMoreText() {
   const moreTextBtn = document.getElementById("read-more-btn");
   moreTextBtn.addEventListener("click", () => {
     content.classList.toggle("moreText-active");
+    if (moreTextBtn.innerHTML == "Läs mer") {
+      moreTextBtn.innerHTML = "Läs mindre";
+    } else {
+      moreTextBtn.innerHTML = "Läs mer";
+    }
   });
 }
 app();
