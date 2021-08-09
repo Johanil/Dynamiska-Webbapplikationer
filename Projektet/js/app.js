e27de64669e7450e292b037db5a67394a7a95cb0
Johan Israelsson -Desktop <johan4411@gmail.com>
2021-08-09 18:03:04 +0200

    Minor fixes with design continues

--- Projektet/js/app.js
+++ Projektet/js/app.js
@@ -3,15 +3,38 @@ import displayWeatherdata from "./display_weatherdata.js";
 function init() {
   displayWeatherdata();
 }
+function app() {
+  slidingNav();
+  scrollToTop();
+  showMoreText();
+  window.addEventListener("load", init);
+}
 
 const slidingNav = () => {
   const burgerBars = document.querySelector(".burgerBars");
   const nav = document.querySelector(".nav_menu");
 
   burgerBars.addEventListener("click", () => {
-      nav.classList.toggle("nav-active");
+    nav.classList.toggle("nav-active");
+    burgerBars.classList.toggle("toggle");
   });
 };
-slidingNav();
+const scrollToTop = () => {
+  const aside = document.querySelector("aside");
+  aside.addEventListener("click", () => {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  });
+};
 
-window.addEventListener("load", init);
+function showMoreText() {
+  const moretext = document.getElementsByName("moreText");
+  const moreTextBtn = document.getElementsByName(".read-more-btn");
+    moreTextBtn.addEventListener("click", () => {
+      
+    });
+};
+app();
