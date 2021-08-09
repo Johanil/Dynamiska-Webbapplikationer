9ab8bf185386a2d39d1938dd9db21ea75f1689f2
Johan Israelsson -Desktop <johan4411@gmail.com>
2021-08-09 14:07:40 +0200

    Dropdown menu css progress

--- Projektet/js/app.js
+++ Projektet/js/app.js
@@ -1,6 +1,17 @@
 import displayWeatherdata from "./display_weatherdata.js";
 
-function init(){
-    displayWeatherdata()
+function init() {
+  displayWeatherdata();
 }
-window.addEventListener('load', init)
+
+const slidingNav = () => {
+  const burgerBars = document.querySelector(".burgerBars");
+  const nav = document.querySelector(".nav_menu");
+
+  burgerBars.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+  });
+};
+slidingNav();
+
+window.addEventListener("load", init);
