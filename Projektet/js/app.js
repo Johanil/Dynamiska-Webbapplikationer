359323a264b1809d36633dbcd0a717da82312539
Johan Israelsson -Desktop <johan4411@gmail.com>
2021-08-10 13:16:31 +0200

    Progress with dropdown menu

--- Projektet/js/app.js
+++ Projektet/js/app.js
@@ -33,13 +33,15 @@ const scrollToTop = () => {
 };
 const hideMenu = () => {
   var lastScrollTop = 0;
-  const navbar = document.querySelector(".header-top");
+    const navbar = document.querySelector(".header-top");
   window.addEventListener("scroll", function () {
     var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     if ((scrollTop < lastScrollTop ) && (window.pageYOffset < "180")) {
-       navbar.style.top = "-5em";
+        navbar.style.top = "-5em";
+        
     } else if (scrollTop > lastScrollTop && window.pageYOffset > "180") {
-      navbar.style.top = "0";
+        navbar.style.top = "0";
+        navbar.classList.toggle(".nav-menu-active");
     }
     lastScrollTop = scrollTop;
   });
